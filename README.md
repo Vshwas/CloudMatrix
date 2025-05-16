be0a9af5ea8ef015a4072aab82f6d4345aa051e4
Vishwas <108745090+Vshwas@users.noreply.github.com>
2025-05-16 16:48:01 +0530

    Python (#2)
    * Create README.md
    
    * example program
    
    --------

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+
+## Python
+=======
 ### python
 
 when you need to add variable togther we will is + symbol to concatenate the two var into one line
 _____________________________________________________________________________________________________
 * if you need to count the variable you need to command = print(len(var))
 * 
+
---
+++ samples.py
@@ -0,0 +1,23 @@
+# Example-1
+# print("hello world")
+
+#example-2
+# print("hello world"/n)------ This code will be exceuted in next line.
+
+#Example-3
+#varaiable
+#We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise. 
+#glass1 = "milk"
+#glass2 = "juice"
+#temp = glass1  
+#glass1 = glass2  
+#glass2 = temp  
+#print("glass1 contains:\n" + glass1)
+#print("glass2 contains:\n" + glass2)
+
+#Example-4
+#print("welcome to the band")
+#city = input("which city u grew up in ?:" "\n")
+#pet = input("name of your pets:" "\n") 
+#print("your band name would be:"  + city +  "" + pet )
+
