a8497447c82f79f37306c42c3a5fe4e83832fa08
Vishwas <108745090+Vshwas@users.noreply.github.com>
2025-05-27 14:23:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-user_name = input("enter your name")
-lenght_name = len(user_name)
-
-print(type("number of letter in your name"))
-print(type("lenght_name"))
-
-print("number of letter in your name:"+ str(lenght_name))
-
+print("vishwas")
