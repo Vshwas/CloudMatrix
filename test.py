e7f92e5375077b1dbb4f4548202b1daeb1dde547
Vishwas Basavaraju (BD/DPA-SAR1) <gsb3kor@bosch.com>
2025-06-04 17:11:26 +0530

    change the program

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-user_name = input("enter your name")
+import os
+
+user_name = os.environ.get("GIVE_USER", "su")
 lenght_name = len(user_name)
 
 print(type("number of letter in your name"))
-print(type("lenght_name"))
+print(type(9.5))
 
 print("number of letter in your name:"+ str(lenght_name))
